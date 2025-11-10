cbaf5b322596cc0463d2604d76e096bc359d3ab2
leonid.medovnikov <leonid.medovnikov@rt.ru>
2025-11-10 18:59:35 +0300

    Temperatures test

--- app/src/main/java/com/example/aichallenge/api/LocalApi.kt
+++ app/src/main/java/com/example/aichallenge/api/LocalApi.kt
@@ -1,6 +1,5 @@
 package com.example.aichallenge.api
 
-import com.example.aichallenge.chat.BotAnswer
 import retrofit2.http.Body
 import retrofit2.http.POST
 
@@ -11,8 +10,5 @@ data class ChatPayload(
 
 interface LocalApi {
     @POST("chat")
-    suspend fun chat(@Body payload: ChatPayload): BotAnswer
-
-    @POST("chat_with_restrictions")
     suspend fun chatWithRestrictions(@Body payload: ChatPayload): String
 }
